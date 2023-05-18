9312108e5c9b52c92c7acbc3c7779d1641ceb9f7
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2023-05-18 15:45:00 -0400

    setup basic app
    time to merge from now on, i hate merging

---
+++ WeatherApp.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherApp {
+
+    public static void main(String[] args) {
+        String city = "London";
+        String apiKey = "0ebb8d189715c75ad5761cf088e67f15";
+
+        try {
+            String weatherData = fetchWeatherData(city, apiKey);
+            double temperature = extractTemperature(weatherData);
+
+            System.out.println("Temperature in " + city + ": " + temperature + "°C");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String fetchWeatherData(String city, String apiKey) throws IOException {
+        String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+
+        return response.toString();
+    }
+
+    private static double extractTemperature(String weatherData) {
+        try {
+            JSONObject json = new JSONObject(weatherData);
+            JSONObject main = json.getJSONObject("main");
+            double temperature = main.getDouble("temp");
+    
+            return temperature;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return 0.0; // Return a default value or handle the error case as needed
+        }
+    }
+}
